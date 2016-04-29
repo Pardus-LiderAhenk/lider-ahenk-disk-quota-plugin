64f4b4410d0f4ba8c714cbfeb30a02cf34c72fa7
mine.dogan <mine.dogan@agem.com.tr>
2016-04-29 16:01:35 +0300

    removed unnecessary widgets

--- lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/dialogs/DiskQuotaProfileDialog.java
+++ lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/dialogs/DiskQuotaProfileDialog.java
@@ -10,7 +10,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
-import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,15 +29,8 @@ public class DiskQuotaProfileDialog implements IProfileDialog {
 	private static final Logger logger = LoggerFactory.getLogger(DiskQuotaProfileDialog.class);
 	
 	// Widgets
-	private Text txtSoftQuota;
-	private Text txtHardQuota;
-	private Text txtDiskUsage;
 	private Spinner spinnerSoftQuota;
 	private Spinner spinnerHardQuota;
-	
-	private String softQuota;
-	private String hardQuota;
-	private String usage;
 	
 	@Override
 	public void init() {
@@ -53,53 +45,12 @@ public class DiskQuotaProfileDialog implements IProfileDialog {
 	private void createQuotaInputs(final Composite parent, final Profile profile) {
 		
 		Composite composite = new Composite(parent, SWT.NONE);
-		composite.setLayout(new GridLayout(6, false));
-		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		Group currentQuote = new Group(composite, SWT.SHADOW_ETCHED_IN);
-		currentQuote.setText(Messages.getString("CURRENT_QUOTA"));
-		currentQuote.setLayout(new GridLayout(2, false));
-		currentQuote.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 2, 3));
-		
-		Label separator = new Label(composite, SWT.VERTICAL | SWT.SEPARATOR);
-	    separator.setLayoutData(new GridData(GridData.FILL_VERTICAL));
+		composite.setLayout(new GridLayout(3, false));
 		
 		Group changeQuote = new Group(composite, SWT.SHADOW_ETCHED_IN);
 		changeQuote.setText(Messages.getString("CHANGE_QUOTA"));
 		changeQuote.setLayout(new GridLayout(3, false));
-		changeQuote.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 3, 2));
-		
-		Label lblSoftQuota = new Label(currentQuote, SWT.NONE);
-		lblSoftQuota.setText(Messages.getString("SOFT_QUOTA"));
-		lblSoftQuota.pack();
-		
-		txtSoftQuota = new Text(currentQuote, SWT.BORDER);
-		txtSoftQuota.setEnabled(false);
-		txtSoftQuota.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
-		//txtSoftQuota.setText(softQuota);
-		txtSoftQuota.pack();
-		
-		Label lblHardQuota = new Label(currentQuote, SWT.NONE);
-		lblHardQuota.setText(Messages.getString("HARD_QUOTA"));
-		lblHardQuota.pack();
-		
-		txtHardQuota = new Text(currentQuote, SWT.BORDER);
-		txtHardQuota.setEnabled(false);
-		txtHardQuota.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
-		//txtHardQuota.setText(hardQuota);
-		txtHardQuota.pack();
-		
-		Label lblDiskUsage = new Label(currentQuote, SWT.NONE);
-		lblDiskUsage.setText(Messages.getString("DISK_USAGE"));
-		lblDiskUsage.pack();
-		
-		txtDiskUsage = new Text(currentQuote, SWT.BORDER);
-		txtDiskUsage.setEnabled(false);
-		txtDiskUsage.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
-		//txtDiskUsage.setText(usage);
-		txtDiskUsage.pack();
-		
-		currentQuote.pack();
+		changeQuote.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 3, 2));
 		
 		Label lblChangeSoftQuota = new Label(changeQuote, SWT.NONE);
 		lblChangeSoftQuota.setText(Messages.getString("SOFT_QUOTA"));
@@ -109,7 +60,7 @@ public class DiskQuotaProfileDialog implements IProfileDialog {
 		spinnerSoftQuota.setMinimum(DiskQuotaConstants.MIN_VALUE);
 		spinnerSoftQuota.setIncrement(1000);
 		spinnerSoftQuota.setMaximum(DiskQuotaConstants.MAX_VALUE);
-		spinnerSoftQuota.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+		spinnerSoftQuota.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		spinnerSoftQuota.pack();
 		
 		if(profile != null && profile.getProfileData() != null) {
@@ -128,7 +79,7 @@ public class DiskQuotaProfileDialog implements IProfileDialog {
 		spinnerHardQuota.setMinimum(DiskQuotaConstants.MIN_VALUE);
 		spinnerHardQuota.setIncrement(1000);
 		spinnerHardQuota.setMaximum(DiskQuotaConstants.MAX_VALUE);
-		spinnerHardQuota.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+		spinnerHardQuota.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		spinnerHardQuota.pack();
 		
 		if(profile != null && profile.getProfileData() != null) {
