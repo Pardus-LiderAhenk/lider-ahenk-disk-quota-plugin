3c47a1a783dcc0dab49e0a68110bf04cd886a87c
mine.dogan <mine.dogan@agem.com.tr>
2016-05-11 12:10:16 +0300

    removed BaseCommand, added plugin info, name and version to command class

--- lider-disk-quota/src/main/java/tr/org/liderahenk/disk/quota/commands/DiskQuotaCommand.java
+++ lider-disk-quota/src/main/java/tr/org/liderahenk/disk/quota/commands/DiskQuotaCommand.java
@@ -2,19 +2,22 @@ package tr.org.liderahenk.disk.quota.commands;
 
 import java.util.ArrayList;
 
+import tr.org.liderahenk.lider.core.api.plugin.ICommand;
 import tr.org.liderahenk.lider.core.api.service.ICommandContext;
 import tr.org.liderahenk.lider.core.api.service.ICommandResult;
 import tr.org.liderahenk.lider.core.api.service.ICommandResultFactory;
 import tr.org.liderahenk.lider.core.api.service.enums.CommandResultStatus;
+import tr.org.liderahenk.disk.quota.plugininfo.PluginInfoImpl;
 
 /**
  * 
  * @author <a href="mailto:mine.dogan@agem.com.tr">Mine Dogan</a>
  *
  */
-public class DiskQuotaCommand extends BaseCommand {
+public class DiskQuotaCommand implements ICommand {
 
 	private ICommandResultFactory resultFactory;
+	private PluginInfoImpl pluginInfo;
 	
 	@Override
 	public ICommandResult execute(ICommandContext context) {
@@ -39,5 +42,19 @@ public class DiskQuotaCommand extends BaseCommand {
 
 	public void setResultFactory(ICommandResultFactory resultFactory) {
 		this.resultFactory = resultFactory;
+	}
+	
+	public void setPluginInfo(PluginInfoImpl pluginInfo) {
+		this.pluginInfo = pluginInfo;
+	}
+
+	@Override
+	public String getPluginName() {
+		return pluginInfo.getPluginName();
+	}
+
+	@Override
+	public String getPluginVersion() {
+		return pluginInfo.getPluginVersion();
 	}
 }
