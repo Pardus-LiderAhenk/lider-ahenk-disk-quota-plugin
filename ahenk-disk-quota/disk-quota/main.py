4e5ba5284e679b941f263ec334ed76351f3515ec
mine.dogan <mine.dogan@agem.com.tr>
2016-10-11 10:19:28 +0300

    main content about developer and description

--- ahenk-disk-quota/disk-quota/main.py
+++ ahenk-disk-quota/disk-quota/main.py
@@ -7,10 +7,10 @@ def info():
     inf['name'] = 'disk-quota'
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
-    inf['description'] = ''
+    inf['description'] = 'Disk-Quota plugin provides to get current soft - hard quota and changing them.'
     inf['task'] = True
     inf['user_oriented'] = True
     inf['machine_oriented'] = False
-    inf['developer'] = ''
+    inf['developer'] = 'mine.dogan@agem.com.tr'
 
     return inf
