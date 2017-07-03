b78edb16934e6b8299690431019a3be5e5657e36
volkansahin <bm.volkansahin@gmail.com>
2017-07-03 09:59:21 +0300

    policy fixed

--- ahenk-disk-quota/disk-quota/policy.py
+++ ahenk-disk-quota/disk-quota/policy.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 # Author:Mine DOGAN <mine.dogan@agem.com.tr>
 
-import os
-import re
-import tempfile
 import json
+import os
+import sys
 
 from base.plugin.abstract_plugin import AbstractPlugin
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
+from fstab import Fstab
 
 
 class DiskQuota(AbstractPlugin):
@@ -29,6 +32,7 @@ class DiskQuota(AbstractPlugin):
 
         self.soft_quota = str(int(self.parameters['soft-quota']) * 1024)
         self.hard_quota = str(int(self.parameters['hard-quota']) * 1024)
+        self.default_quota = str(int(self.parameters['default-quota']) * 1024)
 
         self.logger.debug('Parameters were initialized.')
 
@@ -65,6 +69,7 @@ class DiskQuota(AbstractPlugin):
                                                                                                   self.soft_quota,
                                                                                                   self.hard_quota))
 
+            self.create_default_quota_file()
             self.context.create_response(code=self.get_message_code().POLICY_PROCESSED.value,
                                          message='Kotalar başarıyla güncellendi.')
 
@@ -73,187 +78,8 @@ class DiskQuota(AbstractPlugin):
             self.context.create_response(code=self.get_message_code().POLICY_ERROR.value,
                                          message='Disk Quota profili uygulanırken bir hata oluştu.')
 
-
-class Line(object):
-    """A line in an /etc/fstab line.
-
-      Lines may or may not have a filesystem specification in them. The
-      has_filesystem method tells the user whether they do or not; if they
-      do, the attributes device, directory, fstype, options, dump, and
-      fsck contain the values of the corresponding fields, as instances of
-      the sub-classes of the LinePart class. For non-filesystem lines,
-      the attributes have the None value.
-
-      Lines may or may not be syntactically correct. If they are not,
-      they are treated as as non-filesystem lines.
-
-      """
-
-    # Lines split this way to shut up coverage.py.
-    attrs = ("ws1", "device", "ws2", "directory", "ws3", "fstype")
-    attrs += ("ws4", "options", "ws5", "dump", "ws6", "fsck", "ws7")
-
-    def __init__(self, raw):
-        self.dict = {}
-        self.raw = raw
-
-    def __getattr__(self, name):
-        if name in self.dict:
-            return self.dict[name]
-        else:
-            raise AttributeError(name)
-
-    def __setattr__(self, name, value):
-        forbidden = ("dict", "dump", "fsck", "options")
-        if name not in forbidden and name in self.dict:
-            if self.dict[name] is None:
-                raise Exception("Cannot set attribute %s when line dies not "
-                                "contain filesystem specification" % name)
-            self.dict[name] = value
-        else:
-            object.__setattr__(self, name, value)
-
-    def get_dump(self):
-        return int(self.dict["dump"])
-
-    def set_dump(self, value):
-        self.dict["dump"] = str(value)
-
-    dump = property(get_dump, set_dump)
-
-    def get_fsck(self):
-        return int(self.dict["fsck"])
-
-    def set_fsck(self, value):
-        self.dict["fsck"] = str(value)
-
-    fsck = property(get_fsck, set_fsck)
-
-    def get_options(self):
-        return self.dict["options"].split(",")
-
-    def set_options(self, list):
-        self.dict["options"] = ",".join(list)
-
-    options = property(get_options, set_options)
-
-    def set_raw(self, raw):
-        match = False
-
-        if raw.strip() != "" and not raw.strip().startswith("#"):
-            pat = r"^(?P<ws1>\s*)"
-            pat += r"(?P<device>\S*)"
-            pat += r"(?P<ws2>\s+)"
-            pat += r"(?P<directory>\S+)"
-            pat += r"(?P<ws3>\s+)"
-            pat += r"(?P<fstype>\S+)"
-            pat += r"(?P<ws4>\s+)"
-            pat += r"(?P<options>\S+)"
-            pat += r"(?P<ws5>\s+)"
-            pat += r"(?P<dump>\d+)"
-            pat += r"(?P<ws6>\s+)"
-            pat += r"(?P<fsck>\d+)"
-            pat += r"(?P<ws7>\s*)$"
-
-            match = re.match(pat, raw)
-            if match:
-                self.dict.update((attr, match.group(attr)) for attr in self.attrs)
-
-        if not match:
-            self.dict.update((attr, None) for attr in self.attrs)
-
-        self.dict["raw"] = raw
-
-    def get_raw(self):
-        if self.has_filesystem():
-            return "".join(self.dict[attr] for attr in self.attrs)
-        else:
-            return self.dict["raw"]
-
-    raw = property(get_raw, set_raw)
-
-    def has_filesystem(self):
-        """Does this line have a filesystem specification?"""
-        return self.device is not None
-
-
-class Fstab(object):
-    """An /etc/fstab file."""
-
-    def __init__(self):
-        self.lines = []
-
-    def open_file(self, filespec, mode):
-        if isinstance(filespec, str):
-            return open(filespec, mode=mode)
-        else:
-            return filespec
-
-    def close_file(self, f, filespec):
-        if isinstance(filespec, str):
-            f.close()
-
-    def get_perms(self, filename):
-        return os.stat(filename).st_mode  # pragma: no cover
-
-    def chmod_file(self, filename, mode):
-        os.chmod(filename, mode)  # pragma: no cover
-
-    def link_file(self, oldname, newname):
-        if os.path.exists(newname):
-            os.remove(newname)
-        os.link(oldname, newname)
-
-    def rename_file(self, oldname, newname):
-        os.rename(oldname, newname)  # pragma: no cover
-
-    def read(self, filespec):
-        """Read in a new file.
-
-        If filespec is a string, it is used as a filename. Otherwise
-        it is used as an open file.
-
-        The existing content is replaced.
-
-        """
-
-        f = self.open_file(filespec, "r")
-        lines = []
-        for line in f:
-            lines.append(Line(line))
-        self.lines = lines
-        self.close_file(filespec, f)
-
-    def write(self, filespec):
-        """Write out a new file.
-
-        If filespec is a string, it is used as a filename. Otherwise
-        it is used as an open file.
-
-        """
-
-        if isinstance(filespec, str):
-            # We create the temporary file in the directory (/etc) that the
-            # file exists in. This is so that we can do an atomic rename
-            # later, and that only works inside one filesystem. Some systems
-            # have /tmp and /etc on different filesystems, for good reasons,
-            # and we need to support that.
-            dirname = os.path.dirname(filespec)
-            prefix = os.path.basename(filespec) + "."
-            fd, tempname = tempfile.mkstemp(dir=dirname, prefix=prefix)
-            os.close(fd)
-        else:
-            tempname = filespec
-
-        f = self.open_file(tempname, "w")
-        for line in self.lines:
-            f.write(line.raw)
-        self.close_file(filespec, f)
-
-        if isinstance(filespec, str):
-            self.chmod_file(tempname, self.get_perms(filespec))
-            self.link_file(filespec, filespec + ".bak")
-            self.rename_file(tempname, filespec)
+    def create_default_quota_file(self):
+        self.write_file('default_quota',self.default_quota)
 
 
 def handle_policy(profile_data, context):
