fcfc32342cbe8d4a0126f133105362d5af5d7dc7
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-11-28 11:13:33 +0300

    quata list sort

--- lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/editors/DiskQutaPolicyListEditor.java
+++ lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/editors/DiskQutaPolicyListEditor.java
@@ -1,6 +1,9 @@
 package tr.org.liderahenk.disk.quota.editors;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +18,8 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.osgi.service.runnable.ApplicationLauncher;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -51,7 +56,10 @@ import tr.org.liderahenk.liderconsole.core.utils.IExportableTableViewer;
 import tr.org.liderahenk.liderconsole.core.utils.SWTResourceManager;
 import tr.org.liderahenk.liderconsole.core.widgets.Notifier;
 
+
 public class DiskQutaPolicyListEditor extends EditorPart {
+	public DiskQutaPolicyListEditor() {
+	}
 
 	private static final Logger logger = LoggerFactory.getLogger(DiskQutaPolicyListEditor.class);
 
@@ -64,6 +72,8 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 	private AppliedPolicy selectedPolicy;
 
 	private Button btnSearch;
+	private Text textFilter;
+	private TableFilter tableFilter;
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
@@ -197,6 +207,9 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 				}
 			}
 		});
+		
+		tableFilter = new TableFilter();
+		tableViewer.addFilter(tableFilter);
 		//
 	}
 
@@ -289,17 +302,17 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 			}
 		});
 
-		TableViewerColumn createDateColumn = SWTResourceManager.createTableViewerColumn(tableViewer,
-				Messages.getString("CREATE_DATE"), 100);
-		createDateColumn.setLabelProvider(new ColumnLabelProvider() {
-			@Override
-			public String getText(Object element) {
-				if (element instanceof AppliedPolicy) {
-					return SWTResourceManager.formatDate(((AppliedPolicy) element).getCreateDate());
-				}
-				return Messages.getString("UNTITLED");
-			}
-		});
+//		TableViewerColumn createDateColumn = SWTResourceManager.createTableViewerColumn(tableViewer,
+//				Messages.getString("CREATE_DATE"), 100);
+//		createDateColumn.setLabelProvider(new ColumnLabelProvider() {
+//			@Override
+//			public String getText(Object element) {
+//				if (element instanceof AppliedPolicy) {
+//					return SWTResourceManager.formatDate(((AppliedPolicy) element).getCreateDate());
+//				}
+//				return Messages.getString("UNTITLED");
+//			}
+//		});
 
 		TableViewerColumn applyDateColumn = SWTResourceManager.createTableViewerColumn(tableViewer,
 				Messages.getString("APPLY_DATE"), 100);
@@ -350,7 +363,6 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 
 		// Search label
 		Label lblSearch = new Label(filterContainer, SWT.NONE);
-		lblSearch.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
 		lblSearch.setText(Messages.getString("SEARCH_FILTER"));
 
 		// Filter table rows
@@ -422,6 +434,7 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 		Label lblDesc = new Label(filterContainer, SWT.NONE);
 		lblDesc.setText(Messages.getString("SELECT_USER_OU_DESC"));
 		new Label(filterContainer, SWT.NONE);
+		new Label(filterContainer, SWT.NONE);
 	}
 
 	protected void clearPolicyTable() {
@@ -434,7 +447,7 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 
 		buttonComposite = new Composite(parent, GridData.FILL);
 		buttonComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
-		buttonComposite.setLayout(new GridLayout(3, false));
+		buttonComposite.setLayout(new GridLayout(4, false));
 
 		
 		btnViewDetail = new Button(buttonComposite, SWT.NONE);
@@ -443,6 +456,24 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 		btnViewDetail.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		btnViewDetail.setImage(
 				SWTResourceManager.getImage(LiderConstants.PLUGIN_IDS.LIDER_CONSOLE_CORE, "icons/16/report.png"));
+		
+		textFilter = new Text(buttonComposite, SWT.BORDER);
+		GridData gd_textFilter = new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1);
+		gd_textFilter.widthHint = 158;
+		textFilter.setLayoutData(gd_textFilter);
+		textFilter.setMessage(Messages.getString("search"));
+		
+		textFilter.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent e) {
+				tableFilter.setSearchText(textFilter.getText());
+				tableViewer.refresh();
+			}
+		});
+		
+		
+		new Label(buttonComposite, SWT.NONE);
 		btnViewDetail.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -508,6 +539,15 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 			}
 			
 			
+			Collections.sort(policiesList, new Comparator<AppliedPolicy>() {
+
+				@Override
+				public int compare(AppliedPolicy o1, AppliedPolicy o2) {
+					
+					return o2.getApplyDate().compareTo(o1.getApplyDate());
+				}
+			});
+			
 			tableViewer.setInput(policiesList != null ? policiesList : new ArrayList<AppliedPolicy>());
 			tableViewer.refresh();
 		} catch (Exception e) {
@@ -526,6 +566,28 @@ public class DiskQutaPolicyListEditor extends EditorPart {
 
 	public void setSelectedPolicy(AppliedPolicy selectedPolicy) {
 		this.selectedPolicy = selectedPolicy;
+	}
+	
+	
+	public class TableFilter extends ViewerFilter {
+
+		private String searchString;
+
+		public void setSearchText(String s) {
+			this.searchString = ".*" + s + ".*";
+		}
+
+		@Override
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			if (searchString == null || searchString.length() == 0) {
+				return true;
+			}
+			
+			String uids=StringUtils.join(((AppliedPolicy) element).getUidList(), ",");
+			
+			return uids.matches(searchString);
+		}
+
 	}
 
 }
