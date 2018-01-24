658b06f7c1a198f042f20596754f010b7de7c61a
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-01-24 16:42:34 +0300

    version name fix: 1.1

--- lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/constants/DiskQuotaConstants.java
+++ lider-console-disk-quota/src/tr/org/liderahenk/disk/quota/constants/DiskQuotaConstants.java
@@ -4,7 +4,7 @@ public class DiskQuotaConstants {
 
 	public static final String PLUGIN_NAME = "disk-quota";
 
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.1";
 
 	public static final int MAX_VALUE = 50000; // TODO max value??
 
